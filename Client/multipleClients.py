7f8e2113efbdfcc22d06b4fdac00ee5ec2f16367
JucanAndreiDaniel <setrofex@gmail.com>
2022-06-15 20:48:29 +0300

    update testing script to respect args of client

--- Client/multipleClients.py
+++ Client/multipleClients.py
@@ -3,7 +3,7 @@ import os
 def main(argv):
     n = int(argv[1])
     for i in range(n):
-        os.system("./client send.txt")
+        os.system("./client client 2")
 
 if __name__ == "__main__":
     main(os.sys.argv)
